b893a4e61c2239836f99de16bbab0b710241838f
Oleksandr Smolin <krakensham@gmail.com>
2017-06-17 20:26:27 +0300

    header titles add

--- style.js
+++ style.js
@@ -6,8 +6,24 @@ export default StyleSheet.create({
     'opacity': '0.6',
     'backgroundColor': '#0c0c0c'
   },
+  'logo': {
+    'width': [{ 'unit': 'px', 'value': 82 }],
+    'height': [{ 'unit': 'px', 'value': 32 }]
+  },
+  'title': {
+    'textAlign': 'center'
+  },
+  'little-title': {
+    'height': [{ 'unit': 'px', 'value': 9 }],
+    'fontFamily': 'WorkSans',
+    'fontSize': [{ 'unit': 'px', 'value': 14 }],
+    'fontWeight': 'bold',
+    'lineHeight': [{ 'unit': 'px', 'value': 6 }],
+    'letterSpacing': [{ 'unit': 'px', 'value': 2.8 }],
+    'textAlign': 'center',
+    'color': '#ffffff'
+  },
   'header-title': {
-    'width': [{ 'unit': 'px', 'value': 730 }],
     'height': [{ 'unit': 'px', 'value': 108 }],
     'fontFamily': 'PlayfairDisplay',
     'fontSize': [{ 'unit': 'px', 'value': 48 }],
@@ -19,6 +35,13 @@ export default StyleSheet.create({
   },
   'header-titile-st': {
     'fontWeight': 'normal',
-    'fontStyle': 'italic'
+    'fontStyle': 'italic',
+    'height': [{ 'unit': 'px', 'value': 108 }],
+    'fontFamily': 'PlayfairDisplay',
+    'fontSize': [{ 'unit': 'px', 'value': 48 }],
+    'lineHeight': [{ 'unit': 'px', 'value': 1.25 }],
+    'letterSpacing': [{ 'unit': 'px', 'value': 1.4 }],
+    'textAlign': 'center',
+    'color': '#ffffff'
   }
 });
