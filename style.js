6ec4a0c446c5b30b427c7d32c6f236f722a83673
Oleksandr Smolin <krakensham@gmail.com>
2017-06-17 19:35:40 +0300

    header form add

---
+++ style.js
@@ -0,0 +1,24 @@
+import { StyleSheet } from 'react-native';
+
+export default StyleSheet.create({
+  'header': {
+    'height': [{ 'unit': 'px', 'value': 670 }],
+    'opacity': '0.6',
+    'backgroundColor': '#0c0c0c'
+  },
+  'header-title': {
+    'width': [{ 'unit': 'px', 'value': 730 }],
+    'height': [{ 'unit': 'px', 'value': 108 }],
+    'fontFamily': 'PlayfairDisplay',
+    'fontSize': [{ 'unit': 'px', 'value': 48 }],
+    'fontWeight': 'bold',
+    'lineHeight': [{ 'unit': 'px', 'value': 1.25 }],
+    'letterSpacing': [{ 'unit': 'px', 'value': 1.4 }],
+    'textAlign': 'center',
+    'color': '#ffffff'
+  },
+  'header-titile-st': {
+    'fontWeight': 'normal',
+    'fontStyle': 'italic'
+  }
+});
